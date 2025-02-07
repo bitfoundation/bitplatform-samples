d1274b4e60f53d92bea711623d4c0a840e68670a
ysmoradi <ysmoradi@outlook.com>
2025-02-07 16:30:00 +0100

    Update to bit 9.4.0 (#40)

--- videos/Bit.Besql/Bit.Besql.Sample.Client/Extensions/ServiceCollectionExtensions.cs
+++ videos/Bit.Besql/Bit.Besql.Sample.Client/Extensions/ServiceCollectionExtensions.cs
@@ -8,12 +8,14 @@ public static class ServiceCollectionExtensions
 {
     public static IServiceCollection AddAppServices(this IServiceCollection services)
     {
-        services.AddBesqlDbContextFactory<OfflineDbContext>(optionsBuilder =>
+        services.AddBesqlDbContextFactory<OfflineDbContext>((sp, optionsBuilder) =>
         {
             optionsBuilder
+#if RELEASE
                 .UseModel(OfflineDbContextModel.Instance) // use generated compiled model in order to make db context optimized
+#endif
                 .UseSqlite("Data Source=Offline-ClientDb.db");
-        });
+        }, async (sp, dbContext) => await dbContext.Database.MigrateAsync());
 
         return services;
     }
